f9f940c4012b646b4bbf67e78a9d572340ac8d2c
Jack Neilson <jackdneilson@gmail.com>
2025-02-05 12:36:37 +0000

    Fixed C-c for diagnostics, fixed linting auto update, added lsp_lines for better diagnostics view

---
+++ nvim/lua/plugins/lsp_lines.lua
@@ -0,0 +1,8 @@
+return {
+  "maan2003/lsp_lines.nvim",
+  dependencies = { "neovim/nvim-lspconfig" },
+  config = function()
+    vim.diagnostic.config({ virtual_text = false })
+    require("lsp_lines").setup()
+  end,
+}
