70c9e08c5034d3e449ebcbcf3b819e19e4a1da36
Jack Neilson <jackdneilson@gmail.com>
2025-02-09 17:10:25 +0000

    Fixed neoclip, removed fugitive

--- nvim/lua/plugins/neoclip.lua
+++ nvim/lua/plugins/neoclip.lua
@@ -1,12 +1,14 @@
 return {
   "AckslD/nvim-neoclip.lua",
+  lazy = false,
   dependencies = {
     { 'nvim-telescope/telescope.nvim' },
   },
   config = function()
     require('neoclip').setup()
   end,
-  keys = {
+  keys =
+  {
     { "<leader>fc", "<cmd>Telescope neoclip<CR>", desc = "Find yanks" }
   }
 }
